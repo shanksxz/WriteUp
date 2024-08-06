2616737e6e10f3825edacb0d544175d54c08b423
somya bhatt <somyacu1@gmail.com>
2024-08-06 16:51:29 +0530

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-gray-200 h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
