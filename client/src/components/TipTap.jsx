d4c3b695b4d2f619f555e328b7770d951a81ef18
somya bhatt <somyacu1@gmail.com>
2024-09-02 00:43:09 +0530

    feat: added an rich text editor for content

---
+++ client/src/components/TipTap.jsx
@@ -0,0 +1,36 @@
+import {
+  useEditor,
+  EditorContent,
+  FloatingMenu,
+  BubbleMenu,
+} from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
+import Toolbar from "./ToolBar";
+
+const Tiptap = ({ onChange, content }) => {
+  const handleChange = (newContent) => {
+    onChange(newContent);
+  };
+  const editor = useEditor({
+    extensions: [StarterKit, Underline],
+    editorProps: {
+      attributes: {
+        class:
+          "flex flex-col px-4 py-3 justify-start border-b border-r border-l border-gray-200 text-gray-800 items-start w-full gap-3 font-medium text-[16px] pt-4 rounded-bl-md rounded-br-md outline-none",
+      },
+    },
+    onUpdate: ({ editor }) => {
+      handleChange(editor.getHTML());
+    },
+  });
+
+  return (
+    <div className="w-full">
+      <Toolbar editor={editor} />
+      <EditorContent style={{ whiteSpace: "pre-line" }} editor={editor} />
+    </div>
+  );
+};
+
+export default Tiptap;
