2b494c1b29502c6b008b348c280f155feb630ca2
somya bhatt <somyacu1@gmail.com>
2024-09-01 20:01:27 +0530

    feat: setup routing using rrd

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './components/Login.jsx'
+import Signup from './components/Signup.jsx'
+import Post from './pages/Post.jsx'
+import { AuthProvider } from './context/useAuth.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <App/>
+  },
+  {
+    path : "/auth/login",
+    element : <Login/>
+  },
+  {
+    path : "/auth/signup",
+    element : <Signup />
+  },
+  {
+    path : "/create/post",
+    element : <Post />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AuthProvider>
+    <RouterProvider router={router} />
+  </AuthProvider>
 )
