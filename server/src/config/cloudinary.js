1b9c3f1e9cbbb6c4102bb9ca0429457d1d7ec3bb
somya bhatt <somyacu1@gmail.com>
2024-09-01 19:36:30 +0530

    feat: setup cloudinary for image uploads

---
+++ server/src/config/cloudinary.js
@@ -0,0 +1,45 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import {
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME,
+} from "./config.js";
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfully
+    console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+const deleteFromCloudinary = async (publicId) => {
+  try {
+    if (!publicId) return null;
+    // Delete the file from cloudinary
+    const response = await cloudinary.uploader.destroy(publicId);
+    console.log("File deleted from Cloudinary", response);
+    return response;
+  } catch (error) {
+    console.error("Error deleting file from Cloudinary:", error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,7 +1,6 @@
 import { z } from "zod";
 import dotenv from "dotenv";
 
-// change .env to .env.local if you want to use a local environment file
 dotenv.config({
   path: ".env",
 });
@@ -10,19 +9,17 @@ const configSchema = z.object({
   PORT: z.string().default("3000"),
   DATABASE_URL: z.string(),
   JWT_SECRET: z.string(),
-  // JWT_EXPIRATION: z.string(),
+  CLOUDINARY_CLOUD_NAME: z.string(),
+  CLOUDINARY_API_KEY: z.string(),
+  CLOUDINARY_API_SECRET: z.string(),
 });
 
 const envVars = configSchema.parse(process.env);
 
-// module.exports = {
-//     PORT: envVars.PORT,
-//     DATABASE_URL: envVars.DATABASE_URL,
-//     JWT_SECRET: envVars.JWT_SECRET,
-//     // JWT_EXPIRATION: envVars.JWT_EXPIRATION,
-// }
-
 export const PORT = envVars.PORT;
 export const DATABASE_URL = envVars.DATABASE_URL;
 export const JWT_SECRET = envVars.JWT_SECRET;
+export const CLOUDINARY_CLOUD_NAME = envVars.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = envVars.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = envVars.CLOUDINARY_API_SECRET;
 
