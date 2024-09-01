f3b4ec8f7cb7124984f5b92a02b367d572b705c3
somya bhatt <somyacu1@gmail.com>
2024-09-01 22:18:51 +0530

    feat: added menu for easy navigation

---
+++ client/src/components/DropDownMenu.jsx
@@ -0,0 +1,48 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { useNavigate } from "react-router-dom";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
+export default function DropDownMenu({ user }) {
+  const navigate = useNavigate();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar className="h-7 w-7">
+          <AvatarFallback className="bg-primary text-primary-foreground">
+            {user?.firstName?.split("")[0]}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="rounded-sm"
+        side="bottom"
+        align="end"
+        sideOffset={20}
+      >
+        <DropdownMenuItem onClick={() => navigate("/user/post")}>
+          My Posts
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => navigate("/")}>
+          Explore
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => navigate("/post/create")}>
+          Create Post
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => console.log("DO SHIT")}>
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
